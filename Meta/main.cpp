5217199e6f9f0ba5d3e886fb2142bce3b9ece9ff
Дмитрий Изволов <izvolov@Dmitrij-Izvolovs-MacBook-Pro.local>
2011-12-11 00:30:48 +0400

    Initial Commit

---
+++ Meta/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Meta
+//
+//  Created by Дмитрий Изволов on 11.12.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
