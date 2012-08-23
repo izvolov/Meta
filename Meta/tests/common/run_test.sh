eab6aea2c52607c262a6f68d477ef55e12a30d75
Дмитрий Изволов <dmitriy@izvolov.ru>
2012-08-23 14:15:04 +0400

    Инфраструктура для испытаний.

--- Meta/tests/common/run_test.sh
+++ Meta/tests/common/run_test.sh
@@ -1,7 +1,47 @@
 #!/bin/sh
 
-#  run_test.sh
-#  Meta
-#
-#  Created by Дмитрий Изволов on 15.08.12.
-#
+USAGE="Usage: $0 исходник"
+
+SUCCESS_STRING="\033[32mУспех\033[0m"
+FAIL_STRING="\033[31mПровал\033[0m"
+
+CLEAN_COMPILATION_STRING=ПОРЯДОК
+COMPILE_ERROR_STRING=ОШИБКА
+
+CLEAN_COMPILATION=0
+COMPILE_ERROR=1
+
+if (($# != 1))
+then
+    echo $USAGE
+else
+    SOURCE=$1
+    TEST_NAME=${SOURCE%.*}
+    TIME=`date +%d.%m.%Y-%H:%M:%S`
+    OBJ_FILE_NAME=$TEST_NAME\_$TIME.o
+
+    EXPECTED_RESULT_STRING=`tail -1 $1 | awk '{ print $NF }'`
+    if [ $EXPECTED_RESULT_STRING = $CLEAN_COMPILATION_STRING ];
+    then
+        EXPECTED_RESULT=$CLEAN_COMPILATION
+    elif [ $EXPECTED_RESULT_STRING = $COMPILE_ERROR_STRING ];
+    then
+        EXPECTED_RESULT=$COMPILE_ERROR
+    else
+        echo "Неверный формат ожидаемого результата теста (см. ...)".
+        return 0
+    fi
+
+    clang++ -std=c++0x -stdlib=libc++ -c -I../.. $SOURCE -o $OBJ_FILE_NAME
+    RESULT=$?
+    # ДОДЕЛАТЬ анализ текста выдаваемой ошибки (произошла ли ожидаемая ошибка).
+
+    if [ $RESULT = $EXPECTED_RESULT ];
+    then
+        echo $SUCCESS_STRING
+    else
+        echo $FAIL_STRING
+    fi
+
+    rm $OBJ_FILE_NAME
+fi
