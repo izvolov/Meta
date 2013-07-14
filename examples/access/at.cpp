e42fef32e2b9c0f44fa732e692eff5884d31dea3
Дмитрий Изволов <dmitriy@izvolov.ru>
2013-07-14 21:35:31 +0400

    Начало работы над примерами использования метафункций, а также автосборка примеров и автозапуск испытаний.

---
+++ examples/access/at.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <tuple>
+#include <string>
+
+#include <meta/access/at.hpp>
+
+int main ()
+{
+    typedef std::tuple<size_t, std::string> tuple;
+    typedef meta::at<tuple, 1>::type value_type;
+    value_type string("Это строка.");
+
+    std::cout << string << std::endl;
+
+    return 0;
+}
